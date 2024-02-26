a69b6d453085f0daaa3da827eb1a691f0bfdfb2b
Galymzhan01 <55367155+Galymzhan01@users.noreply.github.com>
2024-02-26 17:59:33 +0600

    Update settings.py

--- baby_cry/settings.py
+++ baby_cry/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&5)#6(1%z9a%o*zj=@@)j6!mr4m=b2qq^huqvl!tji80)-xjhs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['babycry-production.up.railway.app', '34.168.30.222']
+ALLOWED_HOSTS = ['babycry-production.up.railway.app', '34.168.30.222', '*']
 
 
 # Application definition
